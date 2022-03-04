81f6f3a0acdb3f7559357a0f2624069aa132cbef
celebrater <tkgm45nbchk@gmail.com>
2022-03-05 04:55:39 +0900

    added PostRequest class

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,15 +23,21 @@ use App\Http\Controllers\PostController;
 //Route::get('/', 'PostController@index');
 //Route::get('/', 'App\Http\Controllers\PostController@index');
 //Route::get('/', [PostController::class, 'index']);
-Route::get('/posts/{post}', 'PostController@show')->where("post", "[0-9]+");
-Route::get('/posts/create', 'PostController@create');
-Route::post("/posts", "PostController@store");
-Route::get("/posts/{post}/edit", "PostController@edit");
-Route::patch("/posts/{post}", "PostController@update");
-Route::delete("/posts/{post}", "PostController@destroy");
-Route::post("/posts/{post}/comments", "CommentController@store");
-Route::delete("/posts/{post}/comments/{comment}", "CommentController@destroy");
+//Route::get('/posts/{post}', 'PostController@show')->where("post", "[0-9]+");
+//Route::get('/posts/create', 'PostController@create');
+//Route::post("/posts", "PostController@store");
+//Route::get("/posts/{post}/edit", "PostController@edit");
+//Route::patch("/posts/{post}", "PostController@update");
+//Route::delete("/posts/{post}", "PostController@destroy");
+//Route::post("/posts/{post}/comments", "CommentController@store");
+//Route::delete("/posts/{post}/comments/{comment}", "CommentController@destroy");
 
 Route::get('/', [PostController::class, 'index']);
-
+Route::get('/posts/{post}', [PostController::class, 'create'])->where("post", "[0-9]+");
 Route::get('/posts/create', [PostController::class, 'create']);
+Route::post("/posts", [PostController::class, 'create']);
+Route::get("/posts/{post}/edit", [PostController::class, 'edit']);
+Route::patch("/posts/{post}", [PostController::class, 'update']);
+Route::delete("/posts/{post}", [PostController::class, 'destroy']);
+Route::post("/posts/{post}/comments", [CommentController::class, 'store']);
+Route::delete("/posts/{post}/comments/{comment}", [CommentController::class, 'destroy']);
